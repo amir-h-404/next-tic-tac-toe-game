3b400fb4fd34f9d899bc8ab6df92f63fc96f4a04
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-16 10:28:04 -0800

    Add three-step breadcrumb to the top of the homepage

--- app/page.tsx
+++ app/page.tsx
@@ -3,6 +3,7 @@ import {GiTicTacToe} from "react-icons/gi";
 import {HiMiniComputerDesktop} from "react-icons/hi2";
 import {CgBoy} from "react-icons/cg";
 import ButtonWithIcon from "@/app/components/ButtonWithIcon";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
 
 const dynaPuffFont = DynaPuff({
     subsets: ['latin'],
@@ -12,6 +13,16 @@ const dynaPuffFont = DynaPuff({
 export default function Home() {
     return (
         <div>
+            <div className={'p-2 flex gap-2 items-center text-lg mb-5 flex-wrap'}>
+                <span className={'p-2 text-blue-800 underline ' +
+                    'underline-offset-8'}>Choose a game mode</span>
+                <IoIosArrowDroprightCircle className={'text-xl'}/>
+                <span className={'p-2 text-gray-400 cursor-no-drop'}>
+                    Insert information</span>
+                <IoIosArrowDroprightCircle className={'text-xl'}/>
+                <span className={'p-2 text-gray-400 cursor-no-drop'}>
+                    Start the game!</span>
+            </div>
             <div className={'flex flex-col gap-4'}>
                 <GiTicTacToe className={'text-[14rem] text-blue-800 block mx-auto'}/>
                 <h1 className={`${dynaPuffFont.className} text-5xl text-blue-600 
