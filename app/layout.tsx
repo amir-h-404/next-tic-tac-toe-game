cf9589a763526dc5729b69ac97ced31123eb9f99
Amirhossein Emadi <emadi.henker404@gmail.com>
2024-12-16 06:22:02 -0800

    Set default and custom font for the project

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,23 @@
 import type {Metadata} from "next";
 import "./globals.css";
 import React from "react";
+import { JetBrains_Mono } from 'next/font/google';
 
 export const metadata: Metadata = {
     title: "Tic Tac Toe game",
     description: "developed by Amirhossein Emadi"
 };
 
+const jetBrainsMono = JetBrains_Mono({
+    subsets: ['latin'],
+    weight: '600'
+});
+
 export default function RootLayout({
                                        children,
                                    }: Readonly<{ children: React.ReactNode; }>) {
     return (
-        <html lang="en">
+        <html lang="en" className={jetBrainsMono.className}>
             <body>
                 {children}
             </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import {DynaPuff} from 'next/font/google';
+
+const dynaPuffFont = DynaPuff({
+    subsets: ['latin'],
+    weight: '500'
+});
+
 export default function Home() {
     return (
-        <div>Home Page</div>
+        <div className={dynaPuffFont.className}>Home Page</div>
     );
 }
